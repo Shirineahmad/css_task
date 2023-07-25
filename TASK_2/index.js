df0146b54bdc83a055699ddfb037c70fac5607dc
Shirineahmad <shirine.ahmad@hotmail.com>
2023-07-25 15:56:39 +0300

    first commit

---
+++ TASK_2/index.js
@@ -0,0 +1,2 @@
+let myname = "shirine ahmad";
+console.log(myname)
---
+++ TASK_3/index.js
@@ -0,0 +1,16 @@
+const name1 = "shirin";
+var name2 = "ahmad";
+var canPass = true;
+if (canPass) {
+    if (name1 >= name2) {
+        console.log("Hello")
+
+    } else {
+        console.log("Bye")
+    }
+    
+}
+else {
+    console.log("Try again")
+}
+
